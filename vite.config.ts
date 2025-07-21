7c95cbf40e8fc5ae836b375fdce1c8a59314c846
Shujaat Hussain <shujaathussain147@gmail.com>
2025-07-21 20:53:49 +0500

    Final V2

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/LiveMeet-Website/',      // <-- Correct place
+  base: '/LiveMeet.co/',      // <-- Correct place
   server: {
     host: "::",
     port: 8080,
